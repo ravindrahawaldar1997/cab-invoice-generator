b5d498d93d5975b3d433114d4fb82d683dd59971
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-01-02 11:26:20 +0530

    Added: Cab Invoice Generator

---
+++ src/main/java/com/bridgelabz/Calculation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Calculation {
+    public static void main(String[] args) {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        cabInvoiceGenerator.calculateFare(20,60, Rides.RideType.NORMAL);
+        cabInvoiceGenerator.calculateFare(20,60, Rides.RideType.PREMIUM);
+    }
+    }
+
